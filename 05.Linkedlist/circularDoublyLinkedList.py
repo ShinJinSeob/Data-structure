570a81a540b1048d7b07c97345300ad1deff7147
ShinJinSeob <123861270+ShinJinSeob@users.noreply.github.com>
2023-07-04 05:17:30 +0900

    Update circularDoublyLinkedList.py

--- 05.Linkedlist/circularDoublyLinkedList.py
+++ 05.Linkedlist/circularDoublyLinkedList.py
@@ -1,6 +1,8 @@
 from DS.list.bidirectNode import BidirectNode
 
-class CircularDoublyLinkedList:
+class CircularDoublyLinkedList:   
+	# 양방향 원형 연결리스트
+	# 앞뒤로 자유롭게 이동 가능
 	def __init__(self):
 		self.__head = BidirectNode("dummy", None, None)
 		self.__head.prev = self.__head
@@ -142,17 +144,17 @@ class CircularDoublyLinkedList:
 			print(element, end = ' ')
 		print()
  
-	def __iter__(self):  # generating iterator and return
+	def __iter__(self):   # 이터레이터 생성 및 리턴 (for 구문에서 자동 호출)
 		return CircularDoublyLinkedListIterator(self)
- 
+
 class CircularDoublyLinkedListIterator:
-	def __init__(self, alist):
-		self.__head = alist.getNode(-1)  		# 더미 헤드
-		self.iterPosition = self.__head.next  	# 0번 노드
-	def __next__(self):
-		if self.iterPosition == self.__head: 	# 순환 끝
+	def __init__(self, alist):   # (이터레이터가 생성되면서 수행)
+		self.__head = alist.getNode(-1)   # 더미 헤드
+		self.iterPosition = self.__head.next   # 0번 노드
+	def __next__(self):   # (for 구문 in a 에서 계속 수행)
+		if self.iterPosition == self.__head:   # 순환 끝
 			raise StopIteration
-		else: # 현재 원소를 리턴하면서 다음 원소로 이동
+		else:   # 현재 원소를 리턴하면서 다음 원소로 이동
 			item = self.iterPosition.item
 			self.iterPosition = self.iterPosition.next
 			return item
