25aeec666cf02d968ddfa81060f696a8e72ca95e
ShinJinSeob <123861270+ShinJinSeob@users.noreply.github.com>
2023-07-04 07:08:44 +0900

    Update bubbleSort.py

--- 09.Sort/bubbleSort.py
+++ 09.Sort/bubbleSort.py
@@ -1,5 +1,5 @@
 def bubbleSort(A):
-	for numElements in range(len(A), 0, -1):   # 끝 인덱스부터 역순으로 정렬
+	for numElements in range(len(A), 0, -1):   # 끝 원소부터 역순으로 정렬
 		for i in range(numElements-1):   # 앞 원소부터 일일이 비교
 			if A[i] > A[i+1]:
 				A[i], A[i+1] = A[i+1], A[i]
