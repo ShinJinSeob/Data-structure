94554d4ec18c6205664d38208036728fba783af9
ShinJinSeob <123861270+ShinJinSeob@users.noreply.github.com>
2023-07-04 06:51:05 +0900

    Update heap.py

--- 08.Heap/heap.py
+++ 08.Heap/heap.py
@@ -1,41 +1,35 @@
-class Heap:
+class Heap:   # 조건1. 완전 이진 트리,  조건2. 모든 노드는 자식 노드보다 크거나 같음
 	def __init__(self, *args):
 		if len(args) != 0:
 			self.__A = args[0] # 파라미터로 온 리스트
 		else:
 			self.__A = []
  
-	# [알고리즘 8-2] 구현: 힙에 원소 삽입하기(재귀 알고리즘 버전)
-	def insert(self, x):
+	def insert(self, x):   # 삽입
 		self.__A.append(x)
 		self.__percolateUp(len(self.__A)-1)
 
-	# 스며오르기
-	def __percolateUp(self, i:int):
+	def __percolateUp(self, i:int):   # 스며오르기
 		parent = (i - 1) // 2
 		if i > 0 and self.__A[i] > self.__A[parent]:
 			self.__A[i], self.__A[parent] = self.__A[parent], self.__A[i]
 			self.__percolateUp(parent)
 
-	# [알고리즘 8-2] 구현: 힙에서 원소 삭제하기
-	def deleteMax(self):
-		# heap is in self.__A[0...len(self.__A)-1]
+	def deleteMax(self):   # 리프 노드 삭제
 		if (not self.isEmpty()):
 			max = self.__A[0]
-			self.__A[0] = self.__A.pop() # *.pop(): 리스트의 끝원소 삭제 후 원소 리턴
+			self.__A[0] = self.__A.pop()   # 힙의 끝 원소 삭제 후 리프 노드로 리턴
 			self.__percolateDown(0)
 			return max
 		else:
 			return None
 
-	# 스며내리기
-	def __percolateDown(self, i:int):
-		# Percolate down w/ self.__A[i] as the root
-		child = 2 * i + 1  # left child
-		right = 2 * i + 2  # right child
+	def __percolateDown(self, i:int):   # 스며내리기
+		child = 2 * i + 1    # 왼자식
+		right = 2 * i + 2    # 오른자식
 		if (child <= len(self.__A)-1):
 			if (right <= len(self.__A)-1 and self.__A[child] < self.__A[right]):
-				child = right  # index of larger child
+				child = right   # 오른자식을 자식 노드로 변경 (if절 거짓일 시 왼자식 자식 노드로 유지)
 			if self.__A[i] < self.__A[child]:
 				self.__A[i], self.__A[child] = self.__A[child], self.__A[i]
 				self.__percolateDown(child)
@@ -43,12 +37,10 @@ class Heap:
 	def max(self):
 		return self.__A[0]
 
-	# 힙 만들기
-	def buildHeap(self):
+	def buildHeap(self):   # 리스트를 힙으로
 		for i in range((len(self.__A) - 2) // 2, -1, -1):
 			self.__percolateDown(i)
 
-	# 힙이 비었는지 확인하기
 	def isEmpty(self) -> bool:
 		return len(self.__A) == 0
 
