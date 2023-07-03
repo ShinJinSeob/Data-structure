515ae4aa7965cde0b4d4a3a7a7584307a6aacbb4
ShinJinSeob <123861270+ShinJinSeob@users.noreply.github.com>
2023-07-04 07:03:07 +0900

    Update selectionSort.py

--- 09.Sort/selectionSort.py
+++ 09.Sort/selectionSort.py
@@ -1,11 +1,14 @@
 def selectionSort(A):
-	for last in range(len(A)-1, 0, -1):
-		k = theLargest(A, last)	# A[0...last] 중 가장 큰 수 A[k] 찾기
+	for last in range(len(A)-1, 0, -1): 끝 인덱스부터 역순으로 정렬
+		k = theLargest(A, last)	
 		A[k], A[last] =  A[last], A[k]
 
-def theLargest(A, last:int) -> int:	# A[0...last]에서 가장 큰 수의 인덱스를 리턴한다
+def theLargest(A, last:int) -> int:   # 가장 큰 수의 인덱스 리턴
 	largest = 0
 	for i in range(last+1):
 		if A[i] > A[largest]:
 			largest = i
 	return largest
+
+# n^2의 점근적 수행 시간을 가짐 (기본 정렬)
+# 기본 정렬은 삽입, 선택, 버블 순으로 빠름
